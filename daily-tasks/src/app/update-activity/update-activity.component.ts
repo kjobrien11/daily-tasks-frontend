a3e205c1c396b07d7c5f38c588e1a28699599ed4
KJ OBrien <kjokjo219@gmail.com>
2025-09-23 21:12:12 -0400

    applies basic styling

--- daily-tasks/src/app/update-activity/update-activity.component.ts
+++ daily-tasks/src/app/update-activity/update-activity.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-activity',
@@ -8,5 +9,15 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './update-activity.component.css'
 })
 export class UpdateActivityComponent {
+
+  activityForm = new FormGroup({
+    date: new FormControl('', Validators.required),
+    activity: new FormControl('', Validators.required),
+    completionStatus: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    console.log(this.activityForm.value);
+  }
 
 }
