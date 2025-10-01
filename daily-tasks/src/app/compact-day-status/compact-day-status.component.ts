8952685a6b0c52bbac047b9af5fe2d4986b9f966
KJ OBrien <kjokjo219@gmail.com>
2025-09-30 21:23:27 -0400

    adds smaller compact to show more data

---
+++ daily-tasks/src/app/compact-day-status/compact-day-status.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { WeeklyHistory } from '../interfaces/WeekHistory';
+
+@Component({
+  selector: 'app-compact-day-status',
+  imports: [],
+  templateUrl: './compact-day-status.component.html',
+  styleUrl: './compact-day-status.component.css'
+})
+export class CompactDayStatusComponent {
+
+  @Input() data!: WeeklyHistory;
+
+  getBackgroundColor(percentage: number): string {
+  if (percentage === 100) {
+    return '#A9CAB0';
+  } else if (percentage >= 80) {
+    return '#ff9999ff';
+  } else if (percentage >= 60) {
+    return '#ff7070ff';
+  } else if (percentage >= 20) {
+    return '#fe4545ff';
+  } else {
+    return '#fb1919ff';
+  }
+}
+}
--- daily-tasks/src/app/history-popup/history-popup.component.ts
+++ daily-tasks/src/app/history-popup/history-popup.component.ts
@@ -4,14 +4,15 @@ import { DailyActivity } from '../interfaces/DailyActivity';
 import { WeeklyHistory } from '../interfaces/WeekHistory';
 import { MatDialog } from '@angular/material/dialog';
 import { DayStatusComponent } from '../day-status/day-status.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ActivityBreakdownComponent } from '../activity-breakdown/activity-breakdown.component';
-import { ActivityBreakdown } from '../interfaces/ActivityBreakdown';
+import { CompactDayStatusComponent } from '../compact-day-status/compact-day-status.component';
 import { Breakdown } from '../interfaces/Breakdown';
+
 
 @Component({
   selector: 'app-history-popup',
-  imports: [DayStatusComponent, NgFor, ActivityBreakdownComponent],
+  imports: [DayStatusComponent, NgFor, NgIf, ActivityBreakdownComponent, CompactDayStatusComponent],
   templateUrl: './history-popup.component.html',
   styleUrl: './history-popup.component.css'
 })
@@ -20,6 +21,7 @@ export class HistoryPopupComponent implements OnInit {
   rollingData!: WeeklyHistory[];
   rollingDataBreakdown!: Breakdown[];
   averageCompletionPercentage: number = 0;
+  sevenDayMode: boolean = false;
 
   constructor(private activityService: ActivityService) { }
 
